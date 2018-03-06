986f42b31ce69ce97f3435a45d9147b8dc751560
mushfiqur06 <mushfiqur06@gmail.com>
2018-03-06 20:16:12 +0600

    1_52_problem_10.c solve

--- 1_52_problem_10.c
+++ 1_52_problem_10.c
@@ -8,12 +8,11 @@ int main()
     scanf("%d", &t);
 
     while(t--){
-        scanf("%d %d %d", opponent_run, current_run, ball_remaining);
-
+        scanf("%d %d %d", &opponent_run, &current_run, &ball_remaining);
         ball_played = 300 - ball_remaining;
-        current_run_rate = (current_run / ball_played) * 6;
-        asking_run_rate = ((opponent_run - current_run + 1) / ball_remaining) * 6;
-        printf("%0.2lf %0,2lf\n", current_run_rate, asking_run_rate);
+        current_run_rate = (current_run * 1.0 / ball_played) * 6;
+        asking_run_rate = ((opponent_run - current_run + 1) * 1.0 / ball_remaining) * 6;
+        printf("%0.2lf %0.2lf\n", current_run_rate, asking_run_rate);
     }
 
     return 0;
